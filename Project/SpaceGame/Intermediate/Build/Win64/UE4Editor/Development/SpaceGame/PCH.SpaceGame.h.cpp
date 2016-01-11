989c562030d69366a75c5c81f133640a99f16de9
Ossibo <card-gamer@hotmail.com>
2016-01-11 10:37:27 +0100

    Added the Files that did not want to join the fun

--- Project/SpaceGame/Intermediate/Build/Win64/UE4Editor/Development/SpaceGame/PCH.SpaceGame.h.cpp
+++ Project/SpaceGame/Intermediate/Build/Win64/UE4Editor/Development/SpaceGame/PCH.SpaceGame.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\Ossibo\Documents\GitHub\SpaceGame\Project\SpaceGame\Source\SpaceGame\SpaceGame.h"
+#include "C:\Users\Spellabbet\Documents\GitHub\SpaceGame\Project\SpaceGame\Source\SpaceGame\SpaceGame.h"
--- Project/SpaceGame/Intermediate/Build/Win64/UE4Editor/Development/SpaceGame/SpaceGame.h
+++ Project/SpaceGame/Intermediate/Build/Win64/UE4Editor/Development/SpaceGame/SpaceGame.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_SPACEGAME_H__
 #define __AUTO_SPACEGAME_H__
-//Last Write: 2016-01-09 15:19:33 +00:00
-#include "C:\Users\Ossibo\Documents\GitHub\SpaceGame\Project\SpaceGame\Source\SpaceGame\SpaceGame.h"
+//Last Write: 2016-01-11 08:43:00 +00:00
+#include "C:\Users\Spellabbet\Documents\GitHub\SpaceGame\Project\SpaceGame\Source\SpaceGame\SpaceGame.h"
 #endif//__AUTO_SPACEGAME_H__
